dcf884654df8a9c3c5f5301bb5e96cb61f26264d
Arina Okshus <nikita@sushko.ru>
2019-11-08 16:56:10 +0300

    Solved VirtualTurtle, need to refactor

--- 8_eighth/VirtualTurtle.py
+++ 8_eighth/VirtualTurtle.py
@@ -1,9 +1,16 @@
 def turtle(coord, direction):      
-    command = yield coord
-
-    if command == f:
-        coord[direction % 2] += (direction % 2) * (-1)
-    elif command == r:
-        direction = (direction - 1) % 4
-    elif command == l:
-        direction = (direction + 1) % 4
+    while True:
+        command = yield coord
+        if command == 'f':
+            if direction == 0:
+                coord = (coord[0] + 1, coord[1])
+            elif direction == 1:
+                coord = (coord[0], coord[1] + 1)
+            elif direction == 2:
+                coord = (coord[0] - 1, coord[1])
+            else:
+                coord = (coord[0], coord[1] - 1)
+        elif command == 'r':
+            direction = (direction - 1) % 4
+        elif command == 'l':
+            direction = (direction + 1) % 4
