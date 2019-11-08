7fbbf88db42bf41d8888dfc5705387b014668e95
Arina Okshus <nikita@sushko.ru>
2019-11-08 16:40:06 +0300

    Solved SimpleDecorator, started VirtualTurtle

--- 8_eighth/VirtualTurtle.py
+++ 8_eighth/VirtualTurtle.py
@@ -1,1 +1,9 @@
-pass
+def turtle(coord, direction):      
+    command = yield coord
+
+    if command == f:
+        coord[direction % 2] += (direction % 2) * (-1)
+    elif command == r:
+        direction = (direction - 1) % 4
+    elif command == l:
+        direction = (direction + 1) % 4
