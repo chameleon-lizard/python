194489d548cdb76483cdbe2bd967b3d029faa619
Arina Okshus <nikita@sushko.ru>
2019-11-10 18:58:48 +0300

    Solved StatCounter

--- 8_eighth/StatCounter.py
+++ 8_eighth/StatCounter.py
@@ -1,1 +1,12 @@
-pass
+def statcounter():
+    stat = dict()
+    function = yield stat
+
+    while True:
+        def wrapper(*args, function = function):
+            if function in stat:
+                stat[function] += 1
+            else:
+                stat[function] = 1
+            return function(*args)
+        function = yield wrapper
