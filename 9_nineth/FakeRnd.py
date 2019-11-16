36262dfd4fd9c81d575e52b7b27658f1072dd8c2
Arina Okshus <nikita@sushko.ru>
2019-11-16 19:49:34 +0300

    Solved initial test. Fucking hooray.

--- 9_nineth/FakeRnd.py
+++ 9_nineth/FakeRnd.py
@@ -3,7 +3,7 @@ call = -1
 def randint(a, b):
     global call
     call += 1
-    if call % 2:
+    if not call % 2:
         return a
     else:
         return b
@@ -12,7 +12,7 @@ prevs = dict()
 prevs['a'] = 0
 prevs['b'] = 0
 prevs['d'] = 0
-prevs['current'] = 0
+prevs['current'] = 0 
 def randrange(*args):
     if len(args) == 1:
         args = (0, args[0], 1)
@@ -22,7 +22,10 @@ def randrange(*args):
     global prevs
     if args[0] == prevs['a'] and args[1] == prevs['b'] and args[2] == prevs['d']:
         prevs['current'] += prevs['d']
-        return prevs['a'] + prevs['current'] % (prevs['b'] - prevs['a'])
+        if prevs['current'] >= prevs['b']:
+            prevs['current'] = prevs['a'] + prevs['current'] - prevs['b']
+            return prevs['current']
+        return prevs['current']
     else:
         prevs['a'] = args[0]
         prevs['b'] = args[1]
