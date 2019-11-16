8cec92cd22ae7bd5333d8eb9ad73ada512fdfb60
Arina Okshus <nikita@sushko.ru>
2019-11-16 17:20:54 +0300

    In the middle of nowhere...

---
+++ 9_nineth/FakeRnd.py
@@ -0,0 +1,31 @@
+call = -1
+
+def randint(a, b):
+    global call
+    call += 1
+    if call % 2:
+        return a
+    else:
+        return b
+
+prevs = dict()
+prevs['a'] = 0
+prevs['b'] = 0
+prevs['d'] = 0
+prevs['current'] = 0
+def randrange(*args):
+    if len(args) == 1:
+        args = (0, args[0], 1)
+    elif len(args) == 2:
+        args = (args[0], args[1], 1)
+    
+    global prevs
+    if args[0] == prevs['a'] and args[1] == prevs['b'] and args[2] == prevs['d']:
+        prevs['current'] += prevs['d']
+        return prevs['a'] + prevs['current'] % (prevs['b'] - prevs['a'])
+    else:
+        prevs['a'] = args[0]
+        prevs['b'] = args[1]
+        prevs['d'] = args[2]
+        prevs['current'] = prevs['a']
+        return prevs['current']
