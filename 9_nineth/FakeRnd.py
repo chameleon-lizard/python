66281d7461a8865729c36b73cc46b2f852d72b9b
Arina Okshus <nikita@sushko.ru>
2019-11-16 20:21:09 +0300

    IT FUCKING WORKS

--- 9_nineth/FakeRnd.py
+++ 9_nineth/FakeRnd.py
@@ -12,20 +12,31 @@ prevs = dict()
 prevs['a'] = 0
 prevs['b'] = 0
 prevs['d'] = 0
+prevs['trash'] = 0
 prevs['current'] = 0 
 def randrange(*args):
     if len(args) == 1:
-        args = (0, args[0], 1)
+        args = (0, args[0], 1, 0)
     elif len(args) == 2:
-        args = (args[0], args[1], 1)
+        args = (args[0], args[1], 1, 0)
+    elif len(args) == 3:
+        args = (args[0], args[1], args[2], 0)
     
     global prevs
-    if args[0] == prevs['a'] and args[1] == prevs['b'] and args[2] == prevs['d']:
+    if args[0] == prevs['a'] and args[1] == prevs['b'] and args[2] == prevs['d'] and args[3] == prevs['trash']:
         prevs['current'] += prevs['d']
-        if prevs['current'] >= prevs['b']:
-            prevs['current'] = prevs['a'] + prevs['current'] - prevs['b']
-            return prevs['current']
-        return prevs['current']
+        if prevs['d'] > 0:
+            if prevs['current'] >= prevs['b']:
+                prevs['current'] = prevs['a'] + prevs['current'] - prevs['b']
+                return prevs['current']
+            else:
+                return prevs['current']
+        else:
+            if prevs['current'] <= prevs['b']:
+                prevs['current'] = prevs['a'] + (prevs['current'] - prevs['b'])
+                return prevs['current']
+            else:
+                return prevs['current']
     else:
         prevs['a'] = args[0]
         prevs['b'] = args[1]
